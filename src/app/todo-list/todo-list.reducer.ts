8ba073751f777583c2b15e64fb8995eef215aab9
Mike Huang <wellwind@mail2000.com.tw>
2021-06-21 22:41:35 +0800

    設計 Action 要處理的 reducer

--- src/app/todo-list/todo-list.reducer.ts
+++ src/app/todo-list/todo-list.reducer.ts
@@ -1,14 +1,19 @@
+import { TodoItem } from './todo-item';
 import { Action, createReducer, on } from '@ngrx/store';
 import * as TodoListActions from './todo-list.actions';
+import { Pagination } from './pagination';
 
 export const todoListFeatureKey = 'todoList';
 
 export interface State {
-
+  todoItems: Pagination<TodoItem>
 }
 
 export const initialState: State = {
-
+  todoItems: {
+    totalCount: 0,
+    data: []
+  }
 };
 
 
@@ -16,6 +21,16 @@ export const reducer = createReducer(
   initialState,
 
   on(TodoListActions.loadTodoLists, state => state),
+  on(TodoListActions.initTodoListItems, state => ({
+    ...state,
+    todoItems: {
+      totalCount: 2,
+      data: [
+        { id: '1', text: 'Task 1', done: true, created: (new Date()).getTime() },
+        { id: '2', text: 'Task 2', done: true, created: (new Date()).getTime() }
+      ]
+    }
+  })),
 
 );
 
